d8d9dfee288472ad26d92d3bdb03841e74114957
alexTenche <alex.tenche@gmail.com>
2015-03-23 11:07:39 +0200

    first ruby on rails app, yeee

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alex5::Application.config.secret_token = 'd413c01606bf5c04b91764a3582f5823f55bb679b40744df0570f126bd092e0037de37ad3ae7abba663785b265a967f0f3bcd572e09fcc8aef68730f0db51347'
