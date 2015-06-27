ba97d6a18481d6791d0a7aaab1bc399770142e22
Alex Tenche <alex.tenche@gmail.com>
2015-06-27 22:53:01 +0300

    deleted old repo

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Railsblog::Application.config.secret_token = 'a02716d1e6b69e660ca1694a920f4f37e73669ca9071ea1cadbe98894806916f7a420fee7d99b7951a5297a668b6ba0ca56dbaffe60293e3075330cf797305cd'
